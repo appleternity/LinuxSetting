abbd494e4514c19bcfd2ce4bb7fb41b5ff270a8a
Chieh-Yang Huang <chiehyang@psu.edu>
2021-11-18 13:21:21 -0500

    Update install_nvim.sh

--- install_nvim.sh
+++ install_nvim.sh
@@ -4,23 +4,23 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 mv nvim.appimage nvim
-mkdir ~/.config
-mkdir ~/.config/nvim
-mkdir ~/.config/nvim/colors
+mkdir -p ~/.config
+mkdir -p ~/.config/nvim
+mkdir -p ~/.config/nvim/colors
 cp .vimrc ~/.config/nvim/init.vim
 cp monokai.vim ~/.config/nvim/colors/
 nvim +PluginInstall +qall
 
 # >> install to user
-#mkdir ~/.local
-#mkdir ~/.local/bin
+#mkdir -p ~/.local
+#mkdir -p ~/.local/bin
 #cp nvim ~/.local/bin
 #echo 'alias vim=nvim' >> ~/.zshrc
 
 # >> if FUSE doesn't exist
 ./nvim --appimage-extract
-mkdir ~/.local
-mkdir ~/.local/bin
+mkdir -p ~/.local
+mkdir -p ~/.local/bin
 mv squashfs-root ~/.local/nvim
 ln -s ~/.local/nvim/AppRun ~/.local/bin/nvim
 echo 'alias vim=nvim' >> ~/.zshrc
